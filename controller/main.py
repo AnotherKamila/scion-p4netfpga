42a681428d358273b46abc8a7d4def0b61b1f832
Kamila Souckova <kamila@netfpga-scion.inf.ethz.ch>
2019-05-01 16:42:24 +0200

    Add plumbing for exposing NetFPGA registers to Prometheus :-)

--- controller/main.py
+++ controller/main.py
@@ -1,13 +1,30 @@
 import os
 
-from .p4_api import P4Switch
-
+import attr
 from prometheus_client.twisted import MetricsResource
 from twisted.web import resource, server
 from twisted.internet import reactor, endpoints
 
+from .p4_api import P4Switch
+
 PORT = os.getenv('PORT', 8000)
 
+# TODO move somewhere appropriate
+import prometheus_client.core as prom
+
+class P4SwitchCollector:
+    def __init__(self, p4switch):
+        self.p4switch = p4switch
+
+    def collect(self):
+        c = prom.CounterMetricFamily(
+            'p4switch_packets_total',
+            'Total number of packets processed by the P4 pipeline',
+            labels=['interface'],
+            unit='packets',
+        )
+        c.add_metric(['TODO'], self.p4switch.reg_read('stat_counter', 0))
+        yield c
 
 def main():
     http_root = resource.Resource()
@@ -21,8 +38,8 @@ def main():
 
 
 if __name__ == '__main__':
-    switch = P4Switch(
+    p4switch = P4Switch(
         extern_defines='platforms/netfpga/xilinx_stream_switch/sw/CLI/Scion_extern_defines.json'
     )
-    print(switch.reg_read('stat_counter', 0))
+    prom.REGISTRY.register(P4SwitchCollector(p4switch))
     main()
